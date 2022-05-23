7154aefb3fe731e09de328175d8897f7bf21660b
Sarah Frederiksen <sara616b@stud.kea.dk>
2022-05-23 14:23:02 +0200

    more navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,6 @@
 window.addEventListener("DOMContentLoaded", ()=> {
     document.querySelector("#navigation-control").addEventListener("click", (event)=>toggleNav(event));
-    setPositionForSubNav();
 });
-
-window.addEventListener("resize", ()=> setPositionForSubNav())
-
-const setPositionForSubNav = () => {
-    const navText = document.querySelector(".has-sub-navigation");
-    const subNav = document.querySelector("#sub-navigation");
-    subNav.style.top = navText.offsetTop + (navText.offsetHeight /2) + "px";
-    subNav.style.left = navText.offsetLeft + "px";
-0}
 
 const toggleNav = (event) => {
     const idToOpen = event.target.dataset.toOpen;
