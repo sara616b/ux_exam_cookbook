7efe882d123ac5c9d125e742c5bbf6e505056c69
Sarah Frederiksen <sara616b@stud.kea.dk>
2022-05-26 09:01:50 +0200

    more profile (w login)

--- js/profile.js
+++ js/profile.js
@@ -8,4 +8,43 @@ window.addEventListener("DOMContentLoaded", ()=> {
         })
       });  
     })
+    
+    for (let i = 0; i < 6; i++) {
+        const dest = document.querySelector("#recipes-holder");
+        const temp = document.querySelector("#recipe");
+        const clone = temp.cloneNode(true).content;
+        dest.appendChild(clone);
+    }
+
+    // edit / save edit profile
+    document.querySelector("#edit-profile").addEventListener("click", (event)=> startEditing(event));
 });
+
+const startEditing = (event) => {
+  // Change to correct text on button by 'saved' class and toggle class
+  if (event.target.classList.contains("saved")) {
+    event.target.textContent = "Save";
+  } else {
+    event.target.textContent = "Edit profile";
+  };
+  event.target.classList.toggle("saved");
+
+  // find input containers and loop over
+  const inputContainers = document.querySelectorAll(".input-container");
+  inputContainers.forEach((inputContainer) => {
+    // with the class 'not-input' the inputs looks like text
+    inputContainer.classList.toggle("not-input");
+
+    // find input or textarea
+    let inputField = inputContainer.querySelector("input");
+    if (inputField == null) {
+      inputField = inputContainer.querySelector("textarea");
+      if (inputField == null) {
+        return
+      }
+    }
+
+    // enable input
+    inputField.toggleAttribute("disabled")
+  })
+}
