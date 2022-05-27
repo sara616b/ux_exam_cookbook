e09e914da12292fdf2d629844445e28030254424
Sarah Frederiksen <sara616b@stud.kea.dk>
2022-05-27 08:47:53 +0200

    styling and correct some navs and links

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,18 @@
-window.addEventListener("DOMContentLoaded", ()=> {
-    document.querySelector("#navigation-control").addEventListener("click", (event)=>toggleNav(event));
+window.addEventListener("DOMContentLoaded", () => {
+  // open and close burger menu
+  document
+    .querySelector("#navigation-control")
+    .addEventListener("click", (event) => toggleNav(event));
+
+  // nav to search page on search button click
+  document.querySelector(".search-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    window.location.href = "/recipes.html";
+  });
 });
 
 const toggleNav = (event) => {
-    const idToOpen = event.target.dataset.toOpen;
-    document.querySelector("#" + idToOpen).classList.toggle("closed")
+  const idToOpen = event.target.dataset.toOpen;
+  document.querySelector("#" + idToOpen).classList.toggle("closed");
 };
