f092fe4da4baf9bfe556ef666804bbe28e1417bb
Mathias <math92k4@stud.kea.dk>
2022-05-30 08:57:00 +0200

    fp only arrows left

--- js/front.js
+++ js/front.js
@@ -5,6 +5,31 @@ function init() {
   const carouselBtns = document.querySelectorAll(".carousel > button");
   carouselBtns.forEach((btn) => btn.addEventListener("click", moveCarousel));
   if (window.innerWidth > 700) window.addEventListener("resize", resetCarousel);
+
+  //hero slider
+  const heroSlideInterval = setInterval(() => {
+    const container = document.querySelector(".hero-slider");
+    const slider = document.querySelector(".hero-slider .slider");
+    const current = slider.querySelector(".current");
+    const dataNr = Number(current.dataset.nr);
+
+    // Remove old .current's
+    current.classList.remove("current");
+    container.querySelector(".dot.current").classList.remove("current");
+
+    // Set new .current's
+    try {
+      slider.querySelector(`[data-nr="${dataNr + 1}"]`).classList.add("current");
+      container.querySelector(`.dot[data-nr="${dataNr + 1}"]`).classList.add("current");
+
+      slider.style.setProperty("--slide-x", dataNr * -33.33 + "%");
+    } catch {
+      slider.querySelector(`[data-nr="1"]`).classList.add("current");
+      container.querySelector(`.dot[data-nr="1"]`).classList.add("current");
+      slider.style.setProperty("--slide-x", "0%");
+    }
+    console.log("test");
+  }, 5000);
 }
 
 function resetCarousel() {
